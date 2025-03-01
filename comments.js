245c2da24756c1c51feda5ca918499acc8c262e0
sharath <41612121+nayakcm@users.noreply.github.com>
2025-03-01 13:41:26 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,32 +1,59 @@
-// create web server 
-// create a route for comments
-// create a route for new comment
-// create a route for delete comment
+// Create web server 
+// 1. Create a web server
+// 2. Create a route for POST /comments
+// 3. Create a route for GET /comments
+// 4. Create a route for DELETE /comments/:id
+// 5. Create a route for PUT /comments/:id
+// 6. Create a route for PATCH /comments/:id
+// 7. Create a route for GET /comments/:id
 
-var express = require('express');
-var comments = require('./comments');
-var bodyParser = require('body-parser');
-var app = express();
+const express = require('express');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const app = express();
 
-app.get('/comments', function(req, res) {
-    res.json(comments.getComments());
+app.use(express.json());
+
+app.post('/comments', (req, res) => {
+    const comments = JSON.parse(fs.readFileSync('./data/comments.json'));
+    const newComment = req.body;
+    newComment.id = uuidv4();
+    comments.push(newComment);
+    fs.writeFileSync('./data/comments.json', JSON.stringify(comments, null, 2));
+    res.status(201).send(newComment);
 });
 
-app.post('/comments', function(req, res) {
-    var comment = req.body;
-    comments.addComment(comment);
-    res.json(comments.getComments());
+app.get('/comments', (req, res) => {
+    const comments = JSON.parse(fs.readFileSync('./data/comments.json'));
+    res.status(200).send(comments);
 });
 
-app.delete('/comments/:id', function(req, res) {
-    var id = req.params.id;
-    comments.deleteComment(id);
-    res.json(comments.getComments());
+app.delete('/comments/:id', (req, res) => {
+    const comments = JSON.parse(fs.readFileSync('./data/comments.json'));
+    const filteredComments = comments.filter(comment => comment.id !== req.params.id);
+    fs.writeFileSync('./data/comments.json', JSON.stringify(filteredComments, null, 2));
+    res.status(200).send({ message: 'Comment deleted successfully' });
 });
 
-app.listen(3000, function() {
-    console.log('Server is running on http://localhost:3000');
+app.put('/comments/:id', (req, res) => {
+    const comments = JSON.parse(fs.readFileSync('./data/comments.json'));
+    const updatedComments = comments.map(comment => {
+        if (comment.id === req.params.id) {
+            return { ...comment, ...req.body, id: req.params.id };
+        }
+        return comment;
+    });
+    fs.writeFileSync('./data/comments.json', JSON.stringify(updatedComments, null, 2));
+    res.status(200).send({ message: 'Comment updated successfully' });
 });
+
+app.patch('/comments/:id', (req, res) => {
+    const comments = JSON.parse(fs.readFileSync('./data/comments.json'));
+    const updatedComments = comments.map(comment => {
+        if (comment.id === req.params.id) {
+            return { ...comment, ...req.body, id: req.params.id };
+        }
+        return comment;
+    });
+    fs
